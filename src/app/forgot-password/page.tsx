1c5c50153d85ed79ee8d54e6d58962ebd65fa6ca
manojshendge <shendgemanoj878@gmail.com>
2026-07-12 03:14:23 +0530

    file change

--- src/app/forgot-password/page.tsx
+++ src/app/forgot-password/page.tsx
@@ -1,5 +1,11 @@
-import ForgotPasswordPage from "@/components/auth/ForgotPasswordForm";
+// src/app/forgot-password/page.tsx
+import React from "react";
 
-export default function Page() {
-  return <ForgotPasswordPage />;
+export default function ForgotPasswordPage() {
+  return (
+    <div>
+      <h1>Forgot Password</h1>
+      {/* page content */}
+    </div>
+  );
 }
